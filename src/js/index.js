9bcadd7ceea0323a05fdfed206601959e1e11779
LavieMaru <laviemaru@gmail.com>
2017-01-04 17:03:08 -0600

    remove replace with spaces in filter for word wrap

--- src/js/index.js
+++ src/js/index.js
@@ -127,11 +127,10 @@ app.filter('formatText', () => {
   if(!input) return input;
 
   let output = input
+      //replace new lines
       .replace(/(\r\n|\r|\n)/g, '<br/>')
       //replace tabs
       .replace(/\t/g, '&nbsp;&nbsp;&nbsp;')
-      //replace spaces.
-      .replace(/ /g, '&nbsp;');
   return output;
  } 
 });
